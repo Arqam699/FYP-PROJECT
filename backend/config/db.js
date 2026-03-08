22b93573ffaf64c1b0b111e8f0b07b06b36e7d2c
Muhammad Arqam <arqammughal699@gmail.com>
2026-03-08 14:07:22 +0500

    modify

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
-const connectdb = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection failed:", error);
-    }
-}
-export default connectdb;
+import mongoose from 'mongoose';
+const connectdb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL)
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+  }};
+  export default connectdb;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();    
 import connectdb from './config/db.js';
 const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World');
 });
 
 app.listen(port, () => {
-  connectdb();
+ connectdb();
   console.log(`Server is running on port ${port}`);
 });
