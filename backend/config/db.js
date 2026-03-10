c182a5f1bb038768a9be63db419f0cea02b00c17
Muhammad Arqam <arqammughal699@gmail.com>
2026-03-10 22:38:36 +0500

    modified

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-const connectdb = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL);
+// const connectdb = async () => {
+//   try {
+//     console.log('MongoDB connected successfully');
+//   } catch (error) {
+//     console.log('MongoDB connection failed:', error.message);
+//   }
+// };
+ async function connectdb() {
+   try {
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully');
-  } catch (error) {
+} catch (error) {
     console.log('MongoDB connection failed:', error.message);
-  }
-};
+   }
+}
+connectdb();
 export default connectdb;
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import connectdb from './config/db.js';
 import dotenv from 'dotenv';
 import authRouter from './routes/user.routes.js';
 import cookieParser from 'cookie-parser';
+import connectdb from './config/db.js';
 dotenv.config();
 const app = express();
+
   const port = 8000;
 
 app.use(express.json())
 
 app.use(cookieParser())
-
 
 app.use("/api/auth",authRouter)
 
@@ -19,5 +19,4 @@ connectdb();
 
   app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-
 });
