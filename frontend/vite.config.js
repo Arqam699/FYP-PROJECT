c6dfe736c8dd22f1cb1d132d8f3b0bbf58cdb8f4
Muhammad Arqam <arqammughal699@gmail.com>
2026-04-17 14:57:43 +0500

    modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,27 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
-
-  build: {
-    outDir: '../dist'
-  },
-
-  server: {
-    port: 8080,
-  },
-
   plugins: [react(), tailwindcss()],
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: [
-          'import',
-          'mixed-decls',
-          'color-functions',
-          'global-builtin',
-        ],
-      },
-    },
-  },
 })
