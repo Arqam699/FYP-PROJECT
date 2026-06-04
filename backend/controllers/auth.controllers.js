96f492690a6ab93bdddcd8fcd877036ab8b5f1bf
Muhammad Arqam <arqammughal699@gmail.com>
2026-06-04 20:04:24 +0500

    final

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import genToken from "../config/token.js"
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import axios from "axios"
 export const SignUp = async (req,res)=>{
     try{ 
         const {name,email,password} = req.body
@@ -8,13 +9,16 @@ export const SignUp = async (req,res)=>{
             return res.status(400).json({message:"Please fill all the fields"})
         }
         const existEmail = await User.findOne({email})
+
         if(existEmail){
             return res.status(400).json({message:"Email already exists"})
         }
         if(password.length < 6){
             return res.status(400).json({message:"Password must be at least 6 characters"})
         }
+
         const hashPassword = await bcrypt.hash(password,10)
+
         const user = await User.create({
             name,
             email,
@@ -31,12 +35,8 @@ export const SignUp = async (req,res)=>{
     }catch(error){
         console.log(error)
       return res.status(500).json({message:`SignUp error ${error.message}`})
-    }       
+    }
 }
-
-
-
-
 export const Login = async (req,res)=>{
     try{ 
         const {email,password} = req.body
